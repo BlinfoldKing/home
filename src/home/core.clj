6b8a7af85cb96124a2659d7da30707a07c963805
Ganesha Danu <ganeshadanu1@gmail.com>
2020-07-06 15:24:01 +0700

    setup toucan

--- src/home/core.clj
+++ src/home/core.clj
@@ -1,8 +1,15 @@
 (ns home.core
   (:gen-class)
-  (:require [home.config :as config]))
+  (:require [home.config :as config]
+            [toucan.db :as db]
+            [toucan.models :as models]))
+
+(def db-spec {:dbtype   "postgres"
+              :dbname   (config/database-name)
+              :user     (config/database-username)
+              :password (config/database-password)})
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println (config/database-host)))
+  (db/set-default-db-connection! db-spec)
+  (models/set-root-namespace! 'home.models))
---
+++ src/home/models/user.clj
@@ -0,0 +1,4 @@
+(ns home.models.user
+  (:require [toucan.models :refer [defmodel]]))
+
+(defmodel User :users)
